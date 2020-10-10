d20c9014420ab390d190396a49af66092145a24a
jvhti <jvhti@hotmail.com>
2020-10-09 22:01:31 -0300

    Using special lifecycle hook 'getInitialProps'

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Link from "next/link";
 
-const indexPage = () => {
-  return (
-      <div>
-        <h1>The Main Page</h1>
-        <p>Go to <Link href="/auth">Auth</Link></p>
-      </div>
-  );
+class IndexPage extends Component {
+  static async getInitialProps(context){
+    const promise = new Promise((resolve => setTimeout(() => resolve({appName: 'Next App'}), 1000)));
+    return promise;
+  }
+
+  render() {
+    return (
+        <div>
+          <h1>The Main Page of the {this.props.appName}</h1>
+          <p>Go to <Link href="/auth">Auth</Link></p>
+        </div>
+    );
+  }
 };
 
-export default indexPage;
+export default IndexPage;
