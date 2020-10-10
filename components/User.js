9ba83dcd0db55c9248320137109a9de24c4aa59b
jvhti <jvhti@hotmail.com>
2020-10-09 22:05:08 -0300

    Using special lifecycle hook 'getInitialProps' in a functional component

--- components/User.js
+++ components/User.js
@@ -5,6 +5,7 @@ const user = (props) => {
       <div>
         <h1>{props.name}</h1>
         <p>Age: {props.age}</p>
+        <p>Code: {props.code}</p>
         <style jsx>{`
           div{
             border: 1px solid #eee;
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Router from "next/router";
 import User from "../../components/User";
 
-const authIndexPage = () => {
+const authIndexPage = (props) => {
   return (
       <div>
         <h1>The Auth Index Page</h1>
         <button onClick={() => Router.push('/')}>Go Back</button>
-        <User name="João Víctor" age="21"/>
+        <User name="João Víctor" age="21" code={props.code}/>
       </div>
   );
 };
+
+authIndexPage.getInitialProps = async () => new Promise(resolve => setTimeout(() => resolve({code: Math.floor(Math.random() * 1000)}), 2500));
 
 export default authIndexPage;
