31dd667286596524b6f5322602dccdf7ad49fcd5
jvhti <jvhti@hotmail.com>
2020-10-09 21:52:13 -0300

    Create and render a component;
    Same way, next.js doesn't care;

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Router from "next/router";
+import User from "../../components/User";
 
 const authIndexPage = () => {
   return (
       <div>
         <h1>The Auth Index Page</h1>
         <button onClick={() => Router.push('/')}>Go Back</button>
+        <User name="João Víctor" age="21"/>
       </div>
   );
 };
